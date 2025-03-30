894881891397971eb16ea123f6436613d35febe3
ARLU <arludave23@gmail.com>
2025-03-30 15:22:28 +0800

    migration file

--- api/Program.cs
+++ api/Program.cs
@@ -119,6 +119,9 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    await dbContext.Database.MigrateAsync();
+
     var logger = scope.ServiceProvider.GetRequiredService<ILogger<DefaultUsersSeeder>>();
     var seeder = new DefaultUsersSeeder(dbContext, logger);
     await seeder.SeedAsync();
