519b68d06a4fa12e3c4199539868b3358fedaeae
ARLU <arludave23@gmail.com>
2025-03-31 10:09:59 +0800

    Added 2FA Development Code for Convenience

--- api/Models/Entities/TwoFactorAuth.cs
+++ api/Models/Entities/TwoFactorAuth.cs
@@ -8,11 +8,17 @@ namespace api.Models.Entities
     {
         public int Id { get; set; }
         public required string Email { get; set; }
-        public string Code { get; set; } = OTPGenerator.GenerateOTP();
+        public string Code { get; set; } = IsInProductionEnv() ? OTPGenerator.GenerateOTP() : "000000";
+
         public DateTime Expiry { get; set; } = DateTimeUtils.TwoMinutesAfter();
 
         public bool IsExpired => DateTimeUtils.IsExpired(Expiry);
 
         public string Message => $"Your OTP is <strong>{Code}</strong>, and will expire in 2 minutes.";
+
+        private static bool IsInProductionEnv()
+        {
+            return Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production";
+        }
     }
 }
